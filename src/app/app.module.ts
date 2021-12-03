a103afb1ef9e9e6c74efc9a4db847fba5bdc878f
Felix Steiner <felix.steiner7@gmail.com>
2021-12-03 12:20:49 +0100

    Make skaffold

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { InputFieldComponent } from './components/input-field/input-field.component';
+import { InputSelectComponent } from './components/input-select/input-select.component';
+import { InputButtonComponent } from './components/input-button/input-button.component';
+import { ListComponent } from './components/list/list.component';
+import { ListItemComponent } from './components/list-item/list-item.component';
+import { FormComponent } from './components/form/form.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputFieldComponent,
+    InputSelectComponent,
+    InputButtonComponent,
+    ListComponent,
+    ListItemComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
